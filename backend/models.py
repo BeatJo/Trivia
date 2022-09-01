6b9a3b74dd0e4b243bc21491db6408b2a72beeb9
Ivan Mbella <90506108+BeatJo@users.noreply.github.com>
2022-09-01 16:33:02 +0200

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,11 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+user_name = 'postgres'
+password_db = 'azertyuuop'
+host_name = 'localhost:5432'
 database_name = 'trivia'
-database_path = 'postgresql://{}:{}@{}/{}'.format('postgres', 'azertyuuop', 'localhost:5432', database_name)
+database_path = 'postgresql://{}:{}@{}/{}'.format(user_name, password_db, host_name, database_name)
 
 db = SQLAlchemy()
 
